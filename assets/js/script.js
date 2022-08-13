7a59c76ba65ad899dd7eb88f65c12ce34a8318eb
“eliabedossantos” <eliabe.s.o36@gmail.com>
2022-08-13 16:28:45 -0300

    final page version

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ $('#owlTeam').owlCarousel({
     nav:false,
     navText: ["<i class='fas fa-chevron-left' aria-hidden='true'></i>","<i class='fas fa-chevron-right' aria-hidden='true'></i>"],
     dots:true,
+    autoplay:true,
     responsive:{
         0:{
             items:1,
